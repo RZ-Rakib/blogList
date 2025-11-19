df25930844c44845514f704a2d5d3336f59fa53f
Md Rakibuzzaman Rakib <rakibzaman724@gmail.com>
2025-11-19 21:44:37 +0200

    4.4: Helper Functions and Unit Tests, step 2

--- backend/utils/list_helper.js
+++ backend/utils/list_helper.js
@@ -1,6 +1,14 @@
 // eslint-disable-next-line no-unused-vars
-const dummy = (blogs) => {
+const dummy = (_blogs) => {
   return 1
 }
 
-module.exports = { dummy }
+const totalLikes = (blogs) => {
+  const reducer = (totalLikes, currentBlog) => {
+    return totalLikes + currentBlog.likes
+  }
+  return blogs.reduce(reducer, 0)
+}
+
+
+module.exports = { dummy, totalLikes }
