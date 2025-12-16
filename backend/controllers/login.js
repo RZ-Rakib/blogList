83ffac5fcb2cc8d31fb0de44c157a25243772c54
Md Rakibuzzaman Rakib <rakibzaman724@gmail.com>
2025-12-16 10:35:14 +0200

    4.18: Blog List Expansion, step 6

---
+++ backend/controllers/login.js
@@ -0,0 +1,41 @@
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+const logger = require('../utils/logger')
+const { SECRET } = require('../utils/config')
+const loginRoute = require('express').Router()
+
+loginRoute.post('/', async (request, response, next) => {
+  try {
+    const { username, password } = request.body
+
+    const user =  await User.findOne({ username })
+    const passwordCorrect = user === null
+      ? false
+      : await bcrypt.compare(password, user.passwordHash)
+
+    if(!( user && passwordCorrect)) {
+      logger.warn('invalid username or password')
+      return response.status(401).json({ error: 'invalid username or password' })
+    }
+
+    const userForToken = {
+      username: user.username,
+      id: user._id
+    }
+
+    const token = jwt.sign(userForToken, SECRET)
+
+    response
+      .status(201)
+      .json({
+        token,
+        usernamme: user.username,
+        name: user.name
+      })
+  } catch (error) {
+    next(error)
+  }
+})
+
+module.exports = loginRoute
