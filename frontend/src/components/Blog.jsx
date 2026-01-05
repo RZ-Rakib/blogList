0c9a86c94cc63a8e5e2a340cb14e8eb17ae07e87
Md Rakibuzzaman Rakib <rakibzaman724@gmail.com>
2026-01-06 00:03:32 +0200

    5.7 Blog List Frontend, step 7

--- frontend/src/components/Blog.jsx
+++ frontend/src/components/Blog.jsx
@@ -1,7 +1,49 @@
-const Blog = ({ blog }) => (
-  <div>
-    "{blog.title}" - {blog.author}
-  </div>
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const showWhenVisible = { display: visible ? null : 'none' }
+  const hideWhenVisible = { display: visible ? 'none' : null }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 3,
+    paddingBottom: 3,
+    marginBottom: 3,
+    border: 'solid',
+    borderWidth: 1
+  }
+
+  const handleVisible = () => {
+    setVisible(!visible)
+  }
+
+  return (
+    <div style={blogStyle}>
+
+      <div style={hideWhenVisible}>
+        {blog.title}<button onClick={handleVisible}>view</button>
+      </div>
+      <div style={showWhenVisible}>
+        <div>
+          {blog.title}<button onClick={handleVisible}>hide</button>
+        </div>
+        <div>
+          <a href={blog.url} target="_blank" rel="noreferrer">
+            {blog.url}
+          </a>
+          <div>
+            likes:{blog.likes}
+            <button>like</button>
+            {''}
+          </div>
+          {blog.author}
+        </div>
+      </div>
+    </div>
+
+  )
+}
 
 export default Blog
