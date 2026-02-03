2b91cf720b9d6432dcc8e93251c990f313be12f7
Md Rakibuzzaman Rakib <rakibzaman724@gmail.com>
2026-02-04 00:13:06 +0200

    5.17: Blog List End To End Testing, step 2

--- playWright/tests/blog_app.spec.js
+++ playWright/tests/blog_app.spec.js
@@ -1,8 +1,16 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
     await page.goto('/')
+    await request.post('/api/testing/reset')
+    await request.post('/api/users', {
+      data: {
+        username: 'Gorilla',
+        name: 'Rakib',
+        password: 'rz12345'
+      }
+    })
   })
 
   test('Login form is shown', async({ page }) => {
@@ -11,6 +19,29 @@ describe('Blog app', () => {
     await expect(loginForm).toHaveText(/Log in to application/i)
     await expect(loginForm.getByRole('button', { name: 'Login' })).toBeVisible()
     await expect(loginForm).toHaveText(/Don't have any account\?/i)
+  })
 
+  describe('Login', () => {
+    test('success with correct credentials', async ({ page }) => {
+      const usernameInput = page.getByRole('textbox', { name: 'username' })
+      const passwordInput = page.getByRole('textbox', { name: 'password' })
+
+      await usernameInput.fill('Gorilla')
+      await passwordInput.fill('rz12345')
+      await page.getByRole('button', { name: 'Login' }).click()
+
+      await expect(page.getByRole('button', { name: 'logout' }).locator('..').getByText('Rakib logged in')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      const usernameInput = page.getByRole('textbox', { name: 'username' })
+      const passwordInput = page.getByRole('textbox', { name: 'password' })
+
+      await usernameInput.fill('Gorill')
+      await passwordInput.fill('rz12345')
+      await page.getByRole('button', { name: 'Login' }).click()
+
+      await expect(page.getByText('invalid username or password')).toBeVisible()
+    })
   })
 })
